9627e5423c46a4f4f455e3e7ad2bdfd234f884fa
githubhater <unvalued_spotter@proton.me>
2026-01-16 15:08:56 +0500

    added java project

---
+++ src/main/java/com/consoledoom/Main.java
@@ -0,0 +1,8 @@
+package com.consoledoom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("🎮 Console Shooter");
+        System.out.println("Initializing...");
+    }
+}
